ceaf6b101f5919900bb5ed7583df42739075cb0a
SergeyOberemok <sergey.oberemok@gmail.com>
2024-07-07 15:41:03 +0200

    [fix] Fixed virtuals to json. Fixed prettier

--- src/stays/queries/get-stay-duration.handler.ts
+++ src/stays/queries/get-stay-duration.handler.ts
@@ -12,6 +12,6 @@ export class GetStayDurationHandler
   async execute({ payload: { id } }: GetStayDurationQuery) {
     const stay: Stay = await this.staysRepository.findById(id);
 
-    return stay.yearsMonthsDaysDuration;
+    return stay.durations;
   }
 }
